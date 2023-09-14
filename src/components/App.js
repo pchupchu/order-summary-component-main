e84b581af73d937274c20ca591eab132c26784a2
Marina Malyarchuk <pchupchu@yandex.ru>
2023-09-14 16:00:46 +0300

    Add page layout

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Copyright from "./Copyright";
+import OrderSummary from "./OrderSummary";
+
+function App() {
+  return (
+    <div className="App">
+      <OrderSummary />
+      <Copyright />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Copyright.js
@@ -0,0 +1,10 @@
+function Copyright() {
+    return (
+      <div className="attribution">
+        Challenge by <a className="attribution__link" href="https://www.frontendmentor.io?ref=challenge" target="blank">Frontend Mentor</a>. 
+        Coded by <a className="attribution__link" href="https://github.com/pchupchu">Pchupchu</a>.
+      </div>
+    );
+  }
+  
+  export default Copyright;
